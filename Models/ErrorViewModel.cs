a116c1ba4594606da780418faa5ac45fdf609443
IkhsanHadi <ikhsanhdn747@gmail.com>
2025-07-16 14:38:00 +0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EwsBengawanSolo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
